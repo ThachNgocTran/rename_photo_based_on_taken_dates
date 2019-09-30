3aca2ece30ab4d8ac0cd873699d70d867f5dedff
Thach Ngoc TRAN <Thach-Ngoc.TRAN@eurecom.fr>
2019-09-30 22:48:58 +0200

    Improved code quality (try/catch, console input, cleanup...)

--- rename_photo_based_on_taken_dates.py
+++ rename_photo_based_on_taken_dates.py
@@ -1,68 +1,73 @@
 import PIL.Image
-import os
-import os.path
-import shutil
-import os.path
+import shutil, os.path
+from typing import List, Dict
+from argparse import ArgumentParser
+import functools
 
-input_dir = r"Processed"
-output_dir = r"Processed_Out"
-path_date_taken_dict = {}
-date_takens = []
-date_takens_index_dict = {}
+parser = ArgumentParser()
+parser.add_argument("-i", "--input-dir", dest="input_dir", help="Input directory")
+parser.add_argument("-o", "--output-dir", dest="output_dir", default="", help="Output directory")
 
-strange_files = []
+args = parser.parse_args()
+
+input_dir = args.input_dir
+output_dir = "%s_Out" % (input_dir) if len(args.output_dir) == 0 else args.output_dir
+file_path_date_taken_dict: Dict[str, str] = {}
 
 unknown_index = 0
+UNKNOWN_DATE_TAKEN = "UnknownDateTaken"
 
-def get_index():
+def get_index() -> str:
     global unknown_index
     unknown_index += 1
-    return unknown_index
+    return "%s_%s" % (UNKNOWN_DATE_TAKEN, unknown_index)
 
-allowed_exts = ["jpg", "jpeg", "png"]
+allowed_exts = {".jpg", ".jpeg", ".png"}
 EXIF_TAKEN_DATE = 36867
 
-for current_folder, sub_folders, files in os.walk(input_dir):
-    for file in files:
-        file_path = (r"%s\%s" % (current_folder, file)).lower()
-        file_ext = os.path.splitext(file_path)[1]                       # if no extension, still receive ''.
-        if file_ext in allowed_exts:
-            print("Processing [%s]" % (file_path))
-            img = PIL.Image.open(file_path)
-            if isinstance(img, PIL.PngImagePlugin.PngImageFile):        # No "Taken Date" for PNG file.
-                date_taken = "UnknownDateTaken_%s" % (get_index())
-            elif isinstance(img, PIL.JpegImagePlugin.JpegImageFile):
-                exif = img._getexif()
-                if not exif is None and EXIF_TAKEN_DATE in exif:        # Some JPG don't contain EXIF. Some do but doesn't have field "Date Taken".
-                    date_taken = exif[EXIF_TAKEN_DATE]
+print("*** Start ***")
+
+try:
+    for current_folder, sub_folders, files in os.walk(input_dir):
+        for file_path in files:
+            file_path = (r"%s\%s" % (current_folder, file_path)).lower()
+            file_ext = os.path.splitext(file_path)[1]                       # If no extension, still receive ''.
+            if file_ext in allowed_exts:
+                print("Found [%s]..." % (file_path))
+                img = PIL.Image.open(file_path)
+                if isinstance(img, PIL.PngImagePlugin.PngImageFile):        # No "Taken Date" for PNG file.
+                    date_taken = get_index()
+                elif isinstance(img, PIL.JpegImagePlugin.JpegImageFile):
+                    exif = img._getexif()
+                    if not exif is None and EXIF_TAKEN_DATE in exif:        # Some JPG don't contain EXIF. Some do but doesn't have field "Date Taken".
+                        date_taken = exif[EXIF_TAKEN_DATE]
+                    else:
+                        date_taken = get_index()
                 else:
-                    date_taken = "UnknownDateTaken_%s" % (get_index())
-            else:
-                raise ValueError("Unsupported file type.")
-            path_date_taken_dict[file_path] = date_taken
-            date_takens.append(date_taken)
+                    raise ValueError("File type doesn't match file extension. [%s]" % (file_path))
 
-            img.close()
-        else:
-            strange_files.append(file_path)
+                file_path_date_taken_dict[file_path] = date_taken
+                img.close()
 
-if len(strange_files) > 0:
-    print("Those files are not processed: [%s]" % (strange_files))
+    date_takens: List[str] = sorted(file_path_date_taken_dict.values())
+    date_taken_index_dict: Dict[str, int] = {}
 
-date_takens = sorted(date_takens)
+    for idx, date_taken in enumerate(date_takens):
+        date_taken_index_dict[date_taken] = idx
 
-for idx, date_taken in enumerate(date_takens):
-    date_takens_index_dict[date_taken] = idx
+    for (file_path, date_taken) in file_path_date_taken_dict.items():
+        index = date_taken_index_dict[date_taken]
+        file_ext = os.path.splitext(file_path)[1]
+        target_file_name = r"%s\Image_%s%s%s" % (output_dir, index, "_(%s)" % (UNKNOWN_DATE_TAKEN) if date_taken.startswith(UNKNOWN_DATE_TAKEN) else "", file_ext)
+        print("Copying to [%s]..." % (target_file_name))
+        shutil.copy2(file_path, target_file_name)
 
-print("Preparing to copy.")
+    print("Statistics: Found [%s] files. [%s] don't have Taken Date." % \
+          (len(file_path_date_taken_dict.keys()),
+           functools.reduce(lambda val, ele: val + 1 if ele.startswith(UNKNOWN_DATE_TAKEN) else val, file_path_date_taken_dict.values(), 0)))
+    print("*** Executed ok ***")
 
-for  file in path_date_taken_dict.keys():
-    date_taken = path_date_taken_dict[file]
-    index = date_takens_index_dict[date_taken]
-    file_ext = file_ext = os.path.splitext(file)[1]
-    print("Copying [%s]" % (file))
-    if date_taken.startswith("UnknownDateTaken"):
-        target_file_name = r"%s\Image_%s_(UnknownDateTaken)%s" % (output_dir, index, file_ext)
-    else:
-        target_file_name = r"%s\Image_%s%s" % (output_dir, index, file_ext)
-    shutil.copy2(file, target_file_name)
+except Exception as e:
+    print("*** Exception: [%s] ***" % (str(e)))
+
+print("*** End ***")
